249677732ce4110690a40870415560b0f437a666
Andreas Lutro <andreas@hjelseth.com>
2013-08-07 16:03:05 +0200

    added composer.json and app config stuff

---
+++ app/config/app-extra.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Paste into your app.php config file's aliases and serviceproviders.
+ * Remove the ones you don't need.
+ */
+
+	'providers' => array(
+
+		'Authority\AuthorityL4\AuthorityL4ServiceProvider',
+		'Philf\Setting\SettingServiceProvider',
+
+		// local/dev only
+		'Profiler\ProfilerServiceProvider',
+		'Way\Generators\GeneratorsServiceProvider',
+
+	),
+	
+
+	'aliases' => array(
+
+		'Authority' => 'Authority\AuthorityL4\Facades\Authority',
+		'Setting' => 'Philf\Setting\Facades\Setting',
+	
+	),
+
